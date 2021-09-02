e0bc4c291e5d4c56cac98ee1e026bea5c536c2b2
Jake Tanner <tannerjt@g.cofc.edu>
2021-09-02 15:16:19 -0400

    Completed Homework 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: Jake Tanner
+hello.py
+
+Problem: This program tests the virtual environment by printing "hello, world!"
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
